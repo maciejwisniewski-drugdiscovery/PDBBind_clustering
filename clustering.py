3e533ce976eeca2c75312fbb84c1c5e14fd5a90d
maciejwisniewski <m.wisniewski@datascience.edu.pl>
2024-04-17 11:35:08 +0200

    biopython similarity matrix

--- clustering.py
+++ clustering.py
@@ -1,17 +1,21 @@
 import pandas as pd
-from pycdhit import cd_hit, read_clstr
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 from Bio import SeqIO
 import os
-
+import seaborn as sns
+from Bio import pairwise2, Align
+import numpy as np
+from scipy.cluster import hierarchy
+import matplotlib.pyplot as plt
 
 #  Cluster Output Files
-pdbbind_proteins_fasta_output_filepath = '/Users/maciejwisniewski/data/PDBBind_Statistics/Clusters/PDBBind_proteins_sequences.fasta'
-cd_hit_output_filepath = '/Users/maciejwisniewski/data/PDBBind_Statistics/Clusters/PDBBind_proteins_cdhit/'
+pdbbind_proteins_fasta_output_filepath = '/mnt/evafs/groups/sfglab/mwisniewski/ingenix/data/PDBBind_Statistics/Clusters/PDBBind_proteins_sequences.fasta'
+cd_hit_output_filepath = '/mnt/evafs/groups/sfglab/mwisniewski/ingenix/data/PDBBind_Statistics/Clusters/PDBBind_proteins_cdhit'
+biopython_similarity_matrix_filepath= '/mnt/evafs/groups/sfglab/mwisniewski/ingenix/data/PDBBind_Statistics/biopython_pdbbind_sequence_similarity_matrix.npy'
 
 # Wczytanie DataFrame'u zawierającego SMILES ligandów oraz Sekwencje AA białek kompleksów
-dataframe_filepath='/Users/maciejwisniewski/data/PDBBind/LP_PDBBind.csv'
+dataframe_filepath='/mnt/evafs/groups/sfglab/mwisniewski/PhD/data/dataframes/LP_PDBBind.csv'
 dataframe = pd.read_csv(dataframe_filepath)
 
 # Wczytanie sekwencji białek i SMILES
@@ -37,14 +41,22 @@ def merged_fasta(df, protein_merged_seq_output_filepath):
 
     SeqIO.write(protein_records, protein_merged_seq_output_filepath, "fasta")
 
-if not os.path.exists(pdbbind_proteins_fasta_output_filepath):
-    merged_fasta(dataframe, pdbbind_proteins_fasta_output_filepath)
+matrix = Align.substitution_matrices.load("BLOSUM62")
 
-res = cd_hit(
-    i=pdbbind_proteins_fasta_output_filepath,
-    o=cd_hit_output_filepath,
-    c=0.7,
-    d=0,
-    sc=1,
-)
-a=1
+def calculate_similarity(seq1, seq2):
+    alignment = pairwise2.align.globalds(seq1, seq2, matrix, -10, -0.5)[0]
+    aligned_seq1, aligned_seq2, score, _, _ = alignment
+    return score / max(len(seq1), len(seq2))
+
+# Obliczanie macierzy podobieństw
+similarity_matrix = np.zeros((len(protein_sequences), len(protein_sequences)))
+
+for i in range(len(protein_sequences)):
+    for j in range(i, len(protein_sequences)):
+        print(i)
+        similarity_matrix[i][j] = calculate_similarity(protein_sequences[i], protein_sequences[j])
+        similarity_matrix[j][i] = similarity_matrix[i][j]
+
+
+np.save(biopython_similarity_matrix_filepath,similarity_matrix)
+
