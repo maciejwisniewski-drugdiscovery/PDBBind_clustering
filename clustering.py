3ee88ffedc93c0a5af38c127bef715b5cf21f19b
maciejwisniewski <m.wisniewski@datascience.edu.pl>
2024-04-17 12:49:52 +0200

    biopython similarity matrix

--- clustering.py
+++ clustering.py
@@ -1,16 +1,14 @@
 import pandas as pd
-from Bio.Seq import Seq
-from Bio.SeqRecord import SeqRecord
 from Bio import SeqIO
-import os
-import seaborn as sns
-from Bio import pairwise2, Align
-import numpy as np
-from scipy.cluster import hierarchy
-import matplotlib.pyplot as plt
+from Bio.Align import MultipleSeqAlignment
+from Bio.Align.Applications import ClustalOmegaCommandline
+from Bio.Phylo.TreeConstruction import DistanceCalculator
+from Bio.Phylo.TreeConstruction import DistanceTreeConstructor
 
 #  Cluster Output Files
-pdbbind_proteins_fasta_output_filepath = '/mnt/evafs/groups/sfglab/mwisniewski/ingenix/data/PDBBind_Statistics/Clusters/PDBBind_proteins_sequences.fasta'
+pdbbind_proteins_fasta_filepath = '/mnt/evafs/groups/sfglab/mwisniewski/ingenix/data/PDBBind_Statistics/Clusters/PDBBind_proteins_sequences.fasta'
+clustal_align_pdbbind_proteins_filepath = '/mnt/evafs/groups/sfglab/mwisniewski/ingenix/data/PDBBind_Statistics/Clusters/PDBBind_proteins_sequences.fasta'
+
 cd_hit_output_filepath = '/mnt/evafs/groups/sfglab/mwisniewski/ingenix/data/PDBBind_Statistics/Clusters/PDBBind_proteins_cdhit'
 biopython_similarity_matrix_filepath= '/mnt/evafs/groups/sfglab/mwisniewski/ingenix/data/PDBBind_Statistics/biopython_pdbbind_sequence_similarity_matrix.npy'
 
@@ -21,7 +19,10 @@ dataframe = pd.read_csv(dataframe_filepath)
 # Wczytanie sekwencji białek i SMILES
 dataframe['seq'] = dataframe['seq'].str.replace(':','')
 smiles = dataframe['smiles'].tolist()
-protein_sequences = dataframe['seq'].tolist()
+protein_sequences=[]
+for index, row in dataframe.iterrows():
+    protein_sequences.append(SeqIO.SeqRecord(row['seq'], id=row['pdb_id'], description=row['type']))
+
 grouped_sequences = {protein_type: dataframe[dataframe['type'] == protein_type]['seq'].tolist()
                      for protein_type in dataframe['type'].unique()}
 
@@ -41,22 +42,19 @@ def merged_fasta(df, protein_merged_seq_output_filepath):
 
     SeqIO.write(protein_records, protein_merged_seq_output_filepath, "fasta")
 
-matrix = Align.substitution_matrices.load("BLOSUM62")
 
-def calculate_similarity(seq1, seq2):
-    alignment = pairwise2.align.globalds(seq1, seq2, matrix, -10, -0.5)[0]
-    aligned_seq1, aligned_seq2, score, _, _ = alignment
-    return score / max(len(seq1), len(seq2))
+# Tworzymy multiple alignment
+alignment = MultipleSeqAlignment(protein_sequences)
 
-# Obliczanie macierzy podobieństw
-similarity_matrix = np.zeros((len(protein_sequences), len(protein_sequences)))
+# Zapisujemy sekwencje do pliku w formacie FASTA
+SeqIO.write(pdbbind_proteins_fasta_filepath, "sequences.fasta", "fasta")
 
-for i in range(len(protein_sequences)):
-    for j in range(i, len(protein_sequences)):
-        print(i,':',j)
-        similarity_matrix[i][j] = calculate_similarity(protein_sequences[i], protein_sequences[j])
-        similarity_matrix[j][i] = similarity_matrix[i][j]
+# Wywołujemy Clustal Omega do wykonania multiple alignment
+clustalomega_cline = ClustalOmegaCommandline(infile=pdbbind_proteins_fasta_filepath, outfile=clustal_align_pdbbind_proteins_filepath, verbose=True, auto=True)
+stdout, stderr = clustalomega_cline()
 
 
-np.save(biopython_similarity_matrix_filepath,similarity_matrix)
+# Odczytujemy zalignowane sekwencje
+aligned_sequences = SeqIO.parse(clustal_align_pdbbind_proteins_filepath, "fasta")
+
 
