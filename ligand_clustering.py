18df14e399bc641d5640cf455462adb794855ec9
maciejwisniewski <m.wisniewski@datascience.edu.pl>
2024-04-18 20:12:22 +0200

    Ligands Tanimoto

--- ligand_clustering.py
+++ ligand_clustering.py
@@ -5,27 +5,26 @@ import pandas as pd
 import numpy as np
 
 # Wczytanie DataFrame'u zawierającego SMILES ligandów oraz Sekwencje AA białek kompleksów
-#dataframe_filepath='/mnt/evafs/groups/sfglab/mwisniewski/PhD/data/dataframes/LP_PDBBind.csv'
-dataframe_filepath='/Users/maciejwisniewski/data/PDBBind/LP_PDBBind.csv'
-dataframe = pd.read_csv(dataframe_filepath)
-dataframe = dataframe.sort_values(by=['type'])
-types = dataframe['type'].drop_duplicates().tolist()
+dataframe = pd.read_csv(datadir+'/Clusters/clusters/cdhit_protein_sequences_clusters.csv')
 
 # Funkcja do obliczania macierzy podobieństwa
-def calculate_similarity_matrix(smiles_list):
+def calculate_SMILES_similarity_matrix(smiles_list):
     # Konwertowanie SMILES na obiekty molekularne
     mols = [Chem.MolFromSmiles(smiles) for smiles in smiles_list]
-    a=1
+    print(mols)
     # Tworzenie fingerprintów Tanimoto
     fps = [AllChem.GetMorganFingerprintAsBitVect(mol, 2, nBits=1024) for mol in mols]
-
+    print(fps)
     # Obliczanie macierzy podobieństwa
-    similarity_matrix = np.zeros((len(fps), len(fps)))
-    for i in range(len(fps)):
-        for j in range(len(fps)):
-            similarity_matrix[i, j] = DataStructs.TanimotoSimilarity(fps[i], fps[j])
+    similarity_matrix = GetTanimotoSimMat(fps)
+    #similarity_matrix = np.zeros((len(fps), len(fps)))
+    #for i in range(len(fps)):
+    #    for j in range(len(fps)):
+    #        similarity_matrix[i, j] = DataStructs.TanimotoSimilarity(fps[i], fps[j])
     return similarity_matrix
 
 
-similarity_matrix = calculate_similarity_matrix()
-print(similarity_matrix)
+if not os.path.exists(datadir+'/Clusters/matrices/Tanimoto_PDBBind_ligand_SMILES_similarity_matrix.npy'):
+    smiles_list = dataframe['smiles'].tolist()
+    numpy_tanimoto_matrix = calculate_SMILES_similarity_matrix(smiles_list)
+    np.save(datadir+'/Clusters/matrices/Tanimoto_PDBBind_ligand_SMILES_similarity_matrix.npy',numpy_tanimoto_matrix)
