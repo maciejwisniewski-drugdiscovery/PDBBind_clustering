a07b3a47ba4aa0334599f5635546d6187e75027f
maciejwisniewski <m.wisniewski@datascience.edu.pl>
2024-04-18 20:19:29 +0200

    Ligands Tanimoto

--- ligand_clustering.py
+++ ligand_clustering.py
@@ -14,6 +14,8 @@ from pycdhit import read_fasta, CDHIT
 
 from rdkit import Chem
 from rdkit.Chem import AllChem
+from rdkit.Chem import rdFingerprintGenerator
+
 from rdkit.DataManip.Metric.rdMetricMatrixCalc import GetTanimotoSimMat
 
 # Wczytanie DataFrame'u zawierającego SMILES ligandów oraz Sekwencje AA białek kompleksów
@@ -25,19 +27,22 @@ def calculate_SMILES_similarity_matrix(smiles_list):
     # Konwertowanie SMILES na obiekty molekularne
     mols = [Chem.MolFromSmiles(smiles, sanitize=False) for smiles in smiles_list]
     print(mols)
-    # Tworzenie fingerprintów Tanimoto
-    fps = [AllChem.GetMorganFingerprintAsBitVect(mol, 2, nBits=1024) for mol in mols]
-    print(fps)
-    # Obliczanie macierzy podobieństwa
-    similarity_matrix = GetTanimotoSimMat(fps)
-    #similarity_matrix = np.zeros((len(fps), len(fps)))
-    #for i in range(len(fps)):
-    #    for j in range(len(fps)):
-    #        similarity_matrix[i, j] = DataStructs.TanimotoSimilarity(fps[i], fps[j])
-    return similarity_matrix
 
+    # Tworzenie fingerprintów Tanimoto
+    rdkit_gen = rdFingerprintGenerator.GetRDKitFPGenerator(maxPath=7)
+    fgrps = [rdkit_gen.GetFingerprint(mol) for mol in mols]
+
+    #fps = [AllChem.GetMorganFingerprintAsBitVect(mol, 2, nBits=1024) for mol in mols]
+    similarities = np.zeros((len(fgrps), len(fgrps)))
+    for i in range(1, nfgrps):
+        similarity = DataStructs.BulkTanimotoSimilarity(fgrps[i], fgrps[:i])
+        similarities[i, :i] = similarity
+        similarities[:i, i] = similarity
+
+    return similarities
 
 if not os.path.exists(datadir+'/Clusters/matrices/Tanimoto_PDBBind_ligand_SMILES_similarity_matrix.npy'):
     smiles_list = dataframe['smiles'].tolist()
     numpy_tanimoto_matrix = calculate_SMILES_similarity_matrix(smiles_list)
+    print(numpy_tanimoto_matrix)
     np.save(datadir+'/Clusters/matrices/Tanimoto_PDBBind_ligand_SMILES_similarity_matrix.npy',numpy_tanimoto_matrix)
