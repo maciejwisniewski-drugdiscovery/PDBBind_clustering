581ba9951262973ec5e84f6b0c84b78e8bf7d1b7
maciej_wisniewski <m.wisniewski@datascience.edu.pl>
2024-04-18 10:19:21 +0200

    minor changes

--- clustalo_distance_matrix.sh
+++ clustalo_distance_matrix.sh
@@ -5,13 +5,14 @@
 #SBATCH --mem=200G
 #SBATCH --time=24:00:00
 
-input_fasta=/mnt/evafs/groups/sfglab/mwisniewski/ingenix/data/PDBBind_Statistics/Cluster/PDBBind_proteins_sequences.fasta
-distant_matrix_output=/mnt/evafs/groups/sfglab/mwisniewski/ingenix/data/PDBBind_Statistics/Cluster/clustalo_PDBBind_protein_sequences_dsitance_matrix.txt
-
+input_fasta=/mnt/evafs/groups/sfglab/mwisniewski/ingenix/data/PDBBind_Statistics/Clusters/PDBBind_proteins_sequences.fasta
+distant_matrix_output=/mnt/evafs/groups/sfglab/mwisniewski/ingenix/data/PDBBind_Statistics/Clusters/2nd_clustalo_PDBBind_protein_sequences_distance_matrix.txt
+output_clusters=/mnt/evafs/groups/sfglab/mwisniewski/ingenix/data/PDBBind_Statistics/Clusters/clustal_clusters
 
 cd /mnt/evafs/groups/sfglab/mwisniewski/anaconda3/bin
 . activate
 source activate PDBBind_clustering
 cd /mnt/evafs/groups/sfglab/mwisniewski/ingenix/PDBBind_clustering
 
-clustalo -i $input_fasta --distmat-out=$distant_matrix_output --full --force --threads=38
+clustalo -i $input_fasta -o $output_clusters --seqtype=Protein --distmat-out=$distant_matrix_output --full --force --threads=38
+echo 'done'
---
+++ generate_cdhit_clusters.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=cdhit_clusters
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=20
+#SBATCH --mem=100G
+#SBATCH --time=24:00:00
+
+input_fasta=/mnt/evafs/groups/sfglab/mwisniewski/ingenix/data/PDBBind_Statistics/Clusters/PDBBind_proteins_sequences.fasta
+output_clusters=/mnt/evafs/groups/sfglab/mwisniewski/ingenix/data/PDBBind_Statistics/Clusters/cdhit_PDBBind_protein_sequences_clusters
+
+
+cd /mnt/evafs/groups/sfglab/mwisniewski/anaconda3/bin
+. activate
+source activate PDBBind_clustering
+cd /mnt/evafs/groups/sfglab/mwisniewski/ingenix/PDBBind_clustering
+
+cd-hit -i $input_fasta -o $output_clusters -c 0.9 -n 5 -d 0 -T 20
