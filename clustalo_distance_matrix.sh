72dc93e0dbf66a192017f71c1caa01b518659800
maciejwisniewski <m.wisniewski@datascience.edu.pl>
2024-04-18 10:42:51 +0200

    clusters

--- clustalo_distance_matrix.sh
+++ clustalo_distance_matrix.sh
@@ -6,13 +6,13 @@
 #SBATCH --time=24:00:00
 
 input_fasta=/mnt/evafs/groups/sfglab/mwisniewski/ingenix/data/PDBBind_Statistics/Clusters/PDBBind_proteins_sequences.fasta
-distant_matrix_output=/mnt/evafs/groups/sfglab/mwisniewski/ingenix/data/PDBBind_Statistics/Clusters/2nd_clustalo_PDBBind_protein_sequences_distance_matrix.txt
-output_clusters=/mnt/evafs/groups/sfglab/mwisniewski/ingenix/data/PDBBind_Statistics/Clusters/clustal_clusters
+distant_matrix_output=/mnt/evafs/groups/sfglab/mwisniewski/ingenix/data/PDBBind_Statistics/Clusters/clustalo_matrices/clustalo_PDBBind_protein_sequences_distance_matrix.txt
+output_clusters=/mnt/evafs/groups/sfglab/mwisniewski/ingenix/data/PDBBind_Statistics/Clusters/clustalo_clusters/clustalo_clusters
 
 cd /mnt/evafs/groups/sfglab/mwisniewski/anaconda3/bin
 . activate
 source activate PDBBind_clustering
 cd /mnt/evafs/groups/sfglab/mwisniewski/ingenix/PDBBind_clustering
 
-clustalo -i $input_fasta -o $output_clusters --seqtype=Protein --distmat-out=$distant_matrix_output --full --force --threads=38
+clustalo -i $input_fasta --clustering-out $output_clusters --seqtype=Protein --distmat-out=$distant_matrix_output --full --force --threads=38
 echo 'done'
