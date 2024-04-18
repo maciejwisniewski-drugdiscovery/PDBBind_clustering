f7a0a958a5e64e8c060968809bbcb9fb21cd857c
maciejwisniewski <m.wisniewski@datascience.edu.pl>
2024-04-18 19:13:42 +0200

    clustering

--- main.py
+++ main.py
@@ -68,7 +68,7 @@ for protein_type in protein_types:
 a=1
 
 # CD-HiT clustering
-if not os.path.exists(datadir+'/Clusters/clusters/clustalo_protein_sequences_clusters.csv'):
+if not os.path.exists(datadir+'/Clusters/clusters/cdhit_protein_sequences_clusters.csv'):
     for i, protein_type in enumerate(protein_types):
         print(protein_type)
         protein_type_clusters = cluster_proteins_fasta(datadir+'/Clusters/fasta/'+protein_type+'_PDBBind_proteins_sequences.fasta')
