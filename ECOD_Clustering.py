61fa63cbfa837a0e25836ec317ccc94e5eb7852f
maciejwisniewski <m.wisniewski@datascience.edu.pl>
2024-04-23 13:07:19 +0200

    ECOD Clustering

--- ECOD_Clustering.py
+++ ECOD_Clustering.py
@@ -23,7 +23,8 @@ def preprocess_ECOD_df(ECOD_dataframe):
     ECOD_dataframe = ECOD_dataframe[~ECOD_dataframe['ecod_domain_id'].str.contains('e5j3dA3')]
     ECOD_dataframe['pdb_range'] = ECOD_dataframe['pdb_range'].apply(lambda x: x.split(','))
     ECOD_dataframe =ECOD_dataframe.explode(column=['pdb_range'])
-    ECOD_dataframe['pdb_range'] = ECOD_dataframe['pdb_range'].apply(lambda x: x.split(':')[1])
+    print(ECOD_dataframe.head())
+    ECOD_dataframe['pdb_range'] = ECOD_dataframe['pdb_range'].apply(lambda x: x.split(':'))
     return ECOD_dataframe
 
 def mol2_to_biopython_structure(mol2_file):
