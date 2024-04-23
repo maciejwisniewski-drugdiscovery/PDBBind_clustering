0ab7a7a208b681bc9db0938789d5c6933f5d5a0d
maciejwisniewski <m.wisniewski@datascience.edu.pl>
2024-04-23 17:21:05 +0200

    ECOD Clustering

--- ECOD_Clustering.py
+++ ECOD_Clustering.py
@@ -102,6 +102,7 @@ def find_closest_chain_to_ligand(protein_pdb_file,ligand_mol2_file):
     print(ligand_closest_chains)
     count_ligand_closest_chains = Counter(ligand_closest_chains)
     ligand_closest_chain_and_residue = count_ligand_closest_chains.most_common(1)[0][0]
+    print(ligand_closest_chain_and_residue)
 
     return ligand_closest_chain_and_residue
 def check_range(range_tuple, x):
