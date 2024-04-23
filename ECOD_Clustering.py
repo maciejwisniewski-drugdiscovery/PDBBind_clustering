b40804a31b21f3234777cf108b2ad42750e6f915
maciejwisniewski <m.wisniewski@datascience.edu.pl>
2024-04-23 17:19:30 +0200

    ECOD Clustering

--- ECOD_Clustering.py
+++ ECOD_Clustering.py
@@ -92,11 +92,14 @@ def find_closest_chain_to_ligand(protein_pdb_file,ligand_mol2_file):
                                if closest_atom.get_parent().get_full_id()[0] == 'protein']
         count_atom_closest_chains = Counter(atom_closest_chains)
         try:
-            atom_closest_chain = count_atom_closest_chains.most_common(10)
-            print(atom_closest_chain)
-            ligand_closest_chains.append(atom_closest_chain)
+            atom_10_closest_chains = count_atom_closest_chains.most_common(10)
+            for atom_closest_chain in atom_10_closest_chains:
+                if atom_closest_chain[1] > 4:
+                    print(atom_closest_chain)
+                    ligand_closest_chains.append(atom_closest_chain[0])
         except:
             atom_closest_chain = None
+    print(ligand_closest_chains)
     count_ligand_closest_chains = Counter(ligand_closest_chains)
     ligand_closest_chain_and_residue = count_ligand_closest_chains.most_common(1)[0][0]
 
