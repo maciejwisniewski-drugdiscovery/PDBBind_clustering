bc8d847eabc553df99e567cf3e19988a68c2bcf9
maciejwisniewski <m.wisniewski@datascience.edu.pl>
2024-04-24 10:44:43 +0200

    ECOD Clustering

--- ECOD_Clustering.py
+++ ECOD_Clustering.py
@@ -141,7 +141,6 @@ def find_ECOD(molecule,ligand_closest_chain,ligand_closest_residue_id,ECOD_dataf
 print('ECOD Dataframe Preprocessing')
 ECOD_dataframe = preprocess_ECOD_df(ECOD_dataframe)
 
-dataframe = dataframe[dataframe['pdbid']=='3c84']
 for index,row in dataframe.iterrows():
     print(index,'/',len(dataframe))
     molecule = row['pdbid']
