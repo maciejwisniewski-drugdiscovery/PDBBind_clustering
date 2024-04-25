d253507abc010380d329fd990dce929b20bc762f
maciejwisniewski <m.wisniewski@datascience.edu.pl>
2024-04-25 15:24:52 +0200

    Structure Clustering

--- Structure_Clustering.py
+++ Structure_Clustering.py
@@ -22,18 +22,17 @@ for index, rows in dataframe.iterrows():
     protein_1 = get_structure(get_pdb_path(protein_1_filepath))
 
     coords_1, seq_1 = get_residue_data(protein_1)
-    print(seq_1)
+
     for index_2, rows_2 in dataframe[:index+1].iterrows():
         print(index,'--',index_2)
 
         protein_2_filepath = os.path.join(datadir, rows_2['pdbid'] + '_protein')
         protein_2 = get_structure(get_pdb_path(protein_2_filepath))
-        chain_2 = next(protein_2.get_chains())
         coords_2, seq_2 = get_residue_data(protein_2)
 
-        #res = tm_align(coords_1, coords_2, seq_1, seq_2)
-        #print(res.tm_norm_chain1)
-        #print(res.tm_norm_chain2)
+        res = tm_align(coords_1, coords_2, seq_1, seq_2)
+        print(res.tm_norm_chain1)
+        print(res.tm_norm_chain2)
 
 
 
