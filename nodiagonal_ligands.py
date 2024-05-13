71c603bca85624ac0b2d12f8d3872f449283c0d5
maciejwisniewski <m.wisniewski@datascience.edu.pl>
2024-05-13 18:01:36 +0200

    Structure Clustering

--- nodiagonal_ligands.py
+++ nodiagonal_ligands.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-
+import pickle
 import os
 import sys
 
@@ -53,18 +53,35 @@ if not os.path.exists(simi_np_file):
     similarity_matrix = similarity_df.to_numpy()
     np.save('/mnt/evafs/groups/sfglab/mwisniewski/ingenix/data/PDBBind_Statistics/Clusters/matrices/ligand_similarities.npy', similarity_matrix)
 else:
+    similarity_df = pd.read_csv(simi_file)
     similarity_matrix = np.load(simi_np_file)
 
-
+print(similarity_df)
+sys.exit()
 # Which out
 
 ligand_similarity_dict = {}
-for i, row in df.iterrows():
-    similar_ligands_indices = np.where(similarity_matrix[i] > 0.95)[0]
-    print(similar_ligand_indices)
-    df_other_clusters = df[~df['Cluster_4']==row['Cluster_4']]
-    similar_pdb_ids = df_other_clusters.loc[similar_ligands_indices, 'pdbid'].tolist()
-    ligand_similarity_dict[row['pdbid']] = similar_pdb_ids
-    print(ligand_similarity_dict[row['pdbid']])
+ligand_similarity_dict_2 = {}
 
+for i, row in df.iterrows():
+    temp_cluster_df = df
+    temp_indices = temp_cluster_df.index.tolist()
+    temp_matrix = matrix.loc[temp_indices]
+    all = temp_matrix[str(i)]
+    not_all = all[all>0.95]
+    similar_ligands_indices = not_all.index.tolist()
+    similar_ligand_ids = df.iloc[similar_ligands_indices]['pdbid'].tolist()
+    similar_ligand_ecods = df.iloc[similar_ligands_indices]['ECOD_Cluster_4'].tolist()
+    ligand_similarity_dict[row['pdbid']] = {
+        similar_ligand_id:similar_ligand_ecod
+        for similar_ligand_id,similar_ligand_ecod in zip(similar_ligand_ids,similar_ligand_ecods)
+    }
+    print(row['pdbid'],":",ligand_similarity_dict[row['pdbid']])
+
+
+similar_ligands_to_ligand ='/mnt/evafs/groups/sfglab/mwisniewski/ingenix/data/PDBBind_Statistics/Clusters/groups/ligand_similarities.pkl'
+with open(similar_ligands_to_ligand, 'wb') as plik:
+    pickle.dump(ligand_similarity_dict, plik)
+    # transformer trenowany na decoy'ach i wiedzy z krysztalow ktory jednoczenie przewiduje strukture i binding affinity
+    # konsensusowy w tym sensie
 
